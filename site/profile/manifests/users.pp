9d8e76717ccb86244e41835b47a9ecf0fc42ab53
Maxime Boissonneault <maxime.boissonneault@calculquebec.ca>
2024-06-10 16:22:54 -0400

    use sshd_config's augea instead of a file

--- site/profile/manifests/users.pp
+++ site/profile/manifests/users.pp
@@ -174,17 +174,11 @@ define profile::users::local_user (
   }
 
   if $authenticationmethods != '' {
-    file { "/etc/ssh/sshd_config.d/authenticationmethods_${name}.conf":
-      ensure  => file,
-      mode    => '0644',
-      owner   => 'root',
-      group   => 'root',
-      notify  => Service['sshd'],
-      content => @("EOT")
-Match User ${name}
-  AuthenticationMethods ${authenticationmethods}
-Match all
-|EOT
+    sshd_config { "${name} authenticationmethods":
+      ensure    => present,
+      condition => "User ${name}",
+      key       => 'AuthenticationMethods',
+      value     => "${authenticationmethods}"
     }
   }
 }
